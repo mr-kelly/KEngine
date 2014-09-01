d287b9cb8ce04bbcda1cc4e36d61227df288b066
kelly <chepy.v@gmail.com>
2014-09-01 13:16:32 +0800

    read tab example

--- Assets/CosmosEngine/Demo/DemoGame/CMyGame.cs
+++ Assets/CosmosEngine/Demo/DemoGame/CMyGame.cs
@@ -5,12 +5,37 @@ public class CMyGame : MonoBehaviour
 {
     void Awake()
     {
+        CGameSettings.Instance.InitAction += OnGameSettingsInit;
+
         CCosmosEngine.New(
             gameObject,
-            new ICModule[] { },
+            new ICModule[] {
+                CGameSettings.Instance,
+            },
             null,
             null);
 
+        
         CUIManager.Instance.OpenWindow<CUIDemoHome>();
     }
+
+    void OnGameSettingsInit()
+    {
+        CGameSettings _ = CGameSettings.Instance;
+
+        CBase.Log("Begin Load tab file...");
+        _.LoadTab<CTestTabInfo>("setting/test_tab.bytes");
+        CBase.Log("Output the tab file...");
+        foreach (CTestTabInfo info in _.GetInfos<CTestTabInfo>())
+        {
+            CBase.Log("Id:{0}, Name: {1}", info.Id, info.Name);
+        }
+
+    }
+}
+
+public class CTestTabInfo : CBaseInfo
+{
+    // Id auto inherit
+    public string Name;
 }
