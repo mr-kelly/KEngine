a64f81ae471abd53086499faec0d6629252489b6
Kelly <chepy.@gmail.com>
2015-11-21 17:34:24 +0800

    整理命名空间提高兼容性

--- KEngine.UnityProject/Assets/KEngine.Editor/Editor/CommandLine.cs
+++ KEngine.UnityProject/Assets/KEngine.Editor/Editor/CommandLine.cs
@@ -12,99 +12,104 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class CommandArgs
+namespace KEngine.Editor
 {
-	public Dictionary<string, string> ArgPairs
-	{
-		get { return mArgPairs; }
-	}
 
-	public List<string> Params
-	{
-		get { return mParams; }
-	}
 
-	List<string> mParams = new List<string>();
-	Dictionary<string, string> mArgPairs = new Dictionary<string, string>();
-}
+    public class CommandArgs
+    {
+        public Dictionary<string, string> ArgPairs
+        {
+            get { return mArgPairs; }
+        }
 
-public class CommandLine
-{
-	public static CommandArgs Parse(string[] args)
-	{
-		char[] kEqual = new char[] { '=' };
-		char[] kArgStart = new char[] { '-', '\\' };
-		CommandArgs ca = new CommandArgs();
+        public List<string> Params
+        {
+            get { return mParams; }
+        }
 
-		int ii = -1;
-		string token = NextToken(args, ref ii);
+        List<string> mParams = new List<string>();
+        Dictionary<string, string> mArgPairs = new Dictionary<string, string>();
+    }
 
-		while (token != null)
-		{
-			if (IsArg(token))
-			{
-				string arg = token.TrimStart(kArgStart).TrimEnd(kEqual);
-				string value = null;
+    public class CommandLine
+    {
+        public static CommandArgs Parse(string[] args)
+        {
+            char[] kEqual = new char[] { '=' };
+            char[] kArgStart = new char[] { '-', '\\' };
+            CommandArgs ca = new CommandArgs();
 
-				if (arg.Contains("="))
-				{
-					string[] r = arg.Split(kEqual, 2);
+            int ii = -1;
+            string token = NextToken(args, ref ii);
 
-					if (r.Length == 2 && r[1] != string.Empty)
-					{
-						arg = r[0];
-						value = r[1];
-					}
-				}
+            while (token != null)
+            {
+                if (IsArg(token))
+                {
+                    string arg = token.TrimStart(kArgStart).TrimEnd(kEqual);
+                    string value = null;
 
-				while (value == null)
-				{
-					if (ii > args.Length)
-						break;
+                    if (arg.Contains("="))
+                    {
+                        string[] r = arg.Split(kEqual, 2);
 
-					string next = NextToken(args, ref ii);
-					if (next != null)
-					{
-						if (IsArg(next))
-						{
-							ii--;
-							value = "true";
-						}
-						else if (next != "=")
-						{
-							value = next.TrimStart(kEqual);
-						}
-					}
-				}
-				ca.ArgPairs.Add(arg, value);
-			}
-			else if (token != string.Empty)
-			{
-				ca.Params.Add(token);
-			}
+                        if (r.Length == 2 && r[1] != string.Empty)
+                        {
+                            arg = r[0];
+                            value = r[1];
+                        }
+                    }
 
-			token = NextToken(args, ref ii);
-		}
-		return ca;
-	}
+                    while (value == null)
+                    {
+                        if (ii > args.Length)
+                            break;
 
-	static bool IsArg(string arg)
-	{
-		return (arg.StartsWith("-") || arg.StartsWith("\\"));
-	}
+                        string next = NextToken(args, ref ii);
+                        if (next != null)
+                        {
+                            if (IsArg(next))
+                            {
+                                ii--;
+                                value = "true";
+                            }
+                            else if (next != "=")
+                            {
+                                value = next.TrimStart(kEqual);
+                            }
+                        }
+                    }
+                    ca.ArgPairs.Add(arg, value);
+                }
+                else if (token != string.Empty)
+                {
+                    ca.Params.Add(token);
+                }
 
-	static string NextToken(string[] args, ref int ii)
-	{
-		ii++;
-		while (ii < args.Length)
-		{
-			string cur = args[ii].Trim();
-			if (cur != string.Empty)
-			{
-				return cur;
-			}
-			ii++;
-		}
-		return null;
-	}
+                token = NextToken(args, ref ii);
+            }
+            return ca;
+        }
+
+        static bool IsArg(string arg)
+        {
+            return (arg.StartsWith("-") || arg.StartsWith("\\"));
+        }
+
+        static string NextToken(string[] args, ref int ii)
+        {
+            ii++;
+            while (ii < args.Length)
+            {
+                string cur = args[ii].Trim();
+                if (cur != string.Empty)
+                {
+                    return cur;
+                }
+                ii++;
+            }
+            return null;
+        }
+    }
 }
--- KEngine.UnityProject/Assets/KEngine.Editor/Editor/KAutoBuilder.cs
+++ KEngine.UnityProject/Assets/KEngine.Editor/Editor/KAutoBuilder.cs
@@ -18,268 +18,272 @@ using System.IO;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 
-public class KAutoBuilder
+namespace KEngine.Editor
 {
-    static string GetProjectName()
+    public class KAutoBuilder
     {
-        string[] s = Application.dataPath.Split('/');
-        return s[s.Length - 2];
-    }
-
-    static string[] GetScenePaths()
-    {
-        string[] scenes = new string[EditorBuildSettings.scenes.Length];
-
-        for (int i = 0; i < scenes.Length; i++)
+        static string GetProjectName()
         {
-            scenes[i] = EditorBuildSettings.scenes[i].path;
+            string[] s = Application.dataPath.Split('/');
+            return s[s.Length - 2];
         }
 
-        return scenes;
-    }
-
-    static void ParseArgs(ref BuildOptions opt, ref string outputpath)
-    {
-        string[] args = System.Environment.GetCommandLineArgs();
-
-        string productPath = Path.Combine(Application.dataPath, KEngine.AppEngine.GetConfig("ProductRelPath"));
-
-        if (!Directory.Exists(productPath))
+        static string[] GetScenePaths()
         {
-            Directory.CreateDirectory(productPath);
-        }
+            string[] scenes = new string[EditorBuildSettings.scenes.Length];
 
-        if (args.Length >= 2)
-        {
-            CommandArgs commandArg = CommandLine.Parse(args);
-            //List<string> lparams = commandArg.Params;
-            Dictionary<string, string> argPairs = commandArg.ArgPairs;
-
-            foreach (KeyValuePair<string, string> item in argPairs)
+            for (int i = 0; i < scenes.Length; i++)
             {
-                switch (item.Key)
+                scenes[i] = EditorBuildSettings.scenes[i].path;
+            }
+
+            return scenes;
+        }
+
+        static void ParseArgs(ref BuildOptions opt, ref string outputpath)
+        {
+            string[] args = System.Environment.GetCommandLineArgs();
+
+            string productPath = Path.Combine(Application.dataPath, KEngine.AppEngine.GetConfig("ProductRelPath"));
+
+            if (!Directory.Exists(productPath))
+            {
+                Directory.CreateDirectory(productPath);
+            }
+
+            if (args.Length >= 2)
+            {
+                CommandArgs commandArg = CommandLine.Parse(args);
+                //List<string> lparams = commandArg.Params;
+                Dictionary<string, string> argPairs = commandArg.ArgPairs;
+
+                foreach (KeyValuePair<string, string> item in argPairs)
                 {
-                    case "BundleVersion":
-                        PlayerSettings.bundleVersion = item.Value;
-                        break;
-                    case "AndroidVersionCode":
-                        PlayerSettings.Android.bundleVersionCode = System.Int32.Parse(item.Value);
-                        break;
-                    case "AndroidKeyStoreName":
-                        PlayerSettings.Android.keystoreName = item.Value;
-                        break;
-                    case "AndroidKeyStorePass":
-                        PlayerSettings.Android.keystorePass = item.Value;
-                        break;
-                    case "AndroidkeyAliasName":
-                        PlayerSettings.Android.keyaliasName = item.Value;
-                        break;
-                    case "AndroidKeyAliasPass":
-                        PlayerSettings.Android.keyaliasPass = item.Value;
-                        break;
-                    case "BuildOptions":
-                        {
-                            opt = BuildOptions.None;
-                            string[] opts = item.Value.Split('|');
-                            foreach (string o in opts)
+                    switch (item.Key)
+                    {
+                        case "BundleVersion":
+                            PlayerSettings.bundleVersion = item.Value;
+                            break;
+                        case "AndroidVersionCode":
+                            PlayerSettings.Android.bundleVersionCode = System.Int32.Parse(item.Value);
+                            break;
+                        case "AndroidKeyStoreName":
+                            PlayerSettings.Android.keystoreName = item.Value;
+                            break;
+                        case "AndroidKeyStorePass":
+                            PlayerSettings.Android.keystorePass = item.Value;
+                            break;
+                        case "AndroidkeyAliasName":
+                            PlayerSettings.Android.keyaliasName = item.Value;
+                            break;
+                        case "AndroidKeyAliasPass":
+                            PlayerSettings.Android.keyaliasPass = item.Value;
+                            break;
+                        case "BuildOptions":
                             {
-                                opt = opt | (BuildOptions)System.Enum.Parse(typeof(BuildOptions), o);
+                                opt = BuildOptions.None;
+                                string[] opts = item.Value.Split('|');
+                                foreach (string o in opts)
+                                {
+                                    opt = opt | (BuildOptions)System.Enum.Parse(typeof(BuildOptions), o);
+                                }
                             }
-                        }
-                        break;
-                    case "Outputpath":
-                        outputpath = item.Value;
-                        break;
+                            break;
+                        case "Outputpath":
+                            outputpath = item.Value;
+                            break;
+                    }
+                    UnityEngine.Debug.Log(item.Key + " : " + item.Value);
                 }
-                UnityEngine.Debug.Log(item.Key + " : " + item.Value);
             }
         }
-    }
 
-    /// <summary>
-    /// return full path or build
-    /// </summary>
-    /// <param name="outputpath"></param>
-    /// <param name="tag"></param>
-    /// <param name="opt"></param>
-    /// <returns></returns>
-    static string PerformBuild(string outputpath, BuildTarget tag, BuildOptions opt)
-    {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(tag);
-
-        ParseArgs(ref opt, ref outputpath);
-
-        string fullPath = System.IO.Path.Combine(Application.dataPath, System.IO.Path.Combine(KEngine.AppEngine.GetConfig("ProductRelPath"), outputpath));
-
-        string fullDir = System.IO.Path.GetDirectoryName(fullPath);
-
-        if (!Directory.Exists(fullDir))
-            Directory.CreateDirectory(fullDir);
-
-        Logger.Log("Build Client {0} to: {1}", tag, fullPath);
-        BuildPipeline.BuildPlayer(GetScenePaths(), fullPath, tag, opt);
-
-        return fullPath;
-    }
-
-    //public static int GetProgramVersion()
-    //{
-    //    var oldVersion = 0;
-    //    if (File.Exists(GetProgramVersionFullPath()))
-    //        oldVersion = File.ReadAllText(GetProgramVersionFullPath()).ToInt32();
-
-    //    return oldVersion;
-    //}
-
-    //public static string GetProgramVersionFullPath()
-    //{
-    //    string programVersionFile = string.Format("{0}/Resources/ProgramVersion.txt", Application.dataPath);
-    //    return programVersionFile;
-    //}
-
-    [MenuItem("KEngine/AutoBuilder/WindowsX86D")]  // 注意，PC版本放在不一样的目录的！
-    public static void PerformWinBuild()
-    {
-        PerformBuild("ClientX86D.exe", BuildTarget.StandaloneWindows, BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler);
-    }
-
-    //[MenuItem("File/AutoBuilder/WindowsX86")]
-    //static void PerformWinReleaseBuild()
-    //{
-    //	PerformBuild(GetProjectName() + "X86.exe", BuildTarget.StandaloneWindows, BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler);
-    //}
-
-    [MenuItem("KEngine/AutoBuilder/iOS")]
-    public static void PerformiOSBuild()
-    {
-        PerformiOSBuild("App");        
-    }
-
-    public static string PerformiOSBuild(string ipaName, bool isDevelopment = true)
-    {
-        BuildOptions opt = isDevelopment
-            ? (BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler)
-            : BuildOptions.None;
-        return PerformBuild("Apps/IOSProjects/" + ipaName, BuildTarget.iPhone, opt);
-    }
-
-    [MenuItem("KEngine/AutoBuilder/Android")]
-    public static void PerformAndroidBuild()
-    {
-        PerformAndroidBuild("StrikeHero");
-    }
-
-    public static string PerformAndroidBuild(string apkName, bool isDevelopment = true)
-    {
-        BuildOptions opt = isDevelopment
-            ? (BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler)
-            : BuildOptions.None;
-        var path = string.Format("Apps/{0}/{1}.apk", "Android", apkName);
-        return PerformBuild(path, BuildTarget.Android, opt);
-    }
-
-    [MenuItem("KEngine/Clear PC PersitentDataPath")]
-    public static void ClearPersistentDataPath()
-    {
-        foreach (string dir in Directory.GetDirectories(KResourceModule.GetAppDataPath()))
+        /// <summary>
+        /// return full path or build
+        /// </summary>
+        /// <param name="outputpath"></param>
+        /// <param name="tag"></param>
+        /// <param name="opt"></param>
+        /// <returns></returns>
+        static string PerformBuild(string outputpath, BuildTarget tag, BuildOptions opt)
         {
-            Directory.Delete(dir, true);
+            EditorUserBuildSettings.SwitchActiveBuildTarget(tag);
+
+            ParseArgs(ref opt, ref outputpath);
+
+            string fullPath = System.IO.Path.Combine(Application.dataPath, System.IO.Path.Combine(KEngine.AppEngine.GetConfig("ProductRelPath"), outputpath));
+
+            string fullDir = System.IO.Path.GetDirectoryName(fullPath);
+
+            if (!Directory.Exists(fullDir))
+                Directory.CreateDirectory(fullDir);
+
+            Logger.Log("Build Client {0} to: {1}", tag, fullPath);
+            BuildPipeline.BuildPlayer(GetScenePaths(), fullPath, tag, opt);
+
+            return fullPath;
         }
-        foreach (string file in Directory.GetFiles(KResourceModule.GetAppDataPath()))
+
+        //public static int GetProgramVersion()
+        //{
+        //    var oldVersion = 0;
+        //    if (File.Exists(GetProgramVersionFullPath()))
+        //        oldVersion = File.ReadAllText(GetProgramVersionFullPath()).ToInt32();
+
+        //    return oldVersion;
+        //}
+
+        //public static string GetProgramVersionFullPath()
+        //{
+        //    string programVersionFile = string.Format("{0}/Resources/ProgramVersion.txt", Application.dataPath);
+        //    return programVersionFile;
+        //}
+
+        [MenuItem("KEngine/AutoBuilder/WindowsX86D")]  // 注意，PC版本放在不一样的目录的！
+        public static void PerformWinBuild()
         {
-            File.Delete(file);
+            PerformBuild("ClientX86D.exe", BuildTarget.StandaloneWindows, BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler);
         }
-    }
-    [MenuItem("KEngine/Open PC PersitentDataPath Folder")]
-    public static void OpenPersistentDataPath()
-    {
-        System.Diagnostics.Process.Start(KResourceModule.GetAppDataPath());
-    }
 
-    [MenuItem("KEngine/Clear Prefs")]
-    public static void ClearPlayerPrefs()
-    {
-        PlayerPrefs.DeleteAll();
-        PlayerPrefs.Save();
-        CBuildTools.ShowDialog("Prefs Cleared!");
-    }
-}
+        //[MenuItem("File/AutoBuilder/WindowsX86")]
+        //static void PerformWinReleaseBuild()
+        //{
+        //	PerformBuild(GetProjectName() + "X86.exe", BuildTarget.StandaloneWindows, BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler);
+        //}
 
-public class CSymbolLinkHelper
-{
-
-    public static string AssetBundlesLinkPath = "Assets/StreamingAssets/" + KEngine.AppEngine.GetConfig(CCosmosEngineDefaultConfig.StreamingBundlesFolderName) + "/"; // hold asset bundles
-    public static string GetLinkPath()
-    {
-        if (!Directory.Exists(AssetBundlesLinkPath))
-            Directory.CreateDirectory(AssetBundlesLinkPath);
-        return AssetBundlesLinkPath + KResourceModule.BuildPlatformName + "/";
-    }
-
-    public static string GetResourceExportPath()
-    {
-        var resourcePath = CBuildTools.GetExportPath(EditorUserBuildSettings.activeBuildTarget, KResourceModule.Quality);
-        return resourcePath;
-    }
-
-    [MenuItem("KEngine/Symbol Link Builded Resource to StreamingAssets")]
-    public static void SymbolLinkResource()
-    {
-        //CSymbolLinkHelper.DeleteAllLinks(CSymbolLinkHelper.AssetBundlesLinkPath);
-
-        //var exportPath = GetResourceExportPath();
-        //var linkPath = GetLinkPath();
-
-        // TODO: 
-        //CBuildTools.SymbolLinkFolder(exportPath, linkPath);
-
-        Logger.Log("TODO:");
-        AssetDatabase.Refresh();
-    }
-
-    /// <summary>
-    /// 删除指定目录所有链接
-    /// </summary>
-    /// <param name="assetBundlesLinkPath"></param>
-    private static void DeleteAllLinks(string assetBundlesLinkPath)
-    {
-        if (Directory.Exists(assetBundlesLinkPath))
+        [MenuItem("KEngine/AutoBuilder/iOS")]
+        public static void PerformiOSBuild()
         {
-            foreach (var dirPath in Directory.GetDirectories(assetBundlesLinkPath))
+            PerformiOSBuild("App");
+        }
+
+        public static string PerformiOSBuild(string ipaName, bool isDevelopment = true)
+        {
+            BuildOptions opt = isDevelopment
+                ? (BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler)
+                : BuildOptions.None;
+            return PerformBuild("Apps/IOSProjects/" + ipaName, BuildTarget.iPhone, opt);
+        }
+
+        [MenuItem("KEngine/AutoBuilder/Android")]
+        public static void PerformAndroidBuild()
+        {
+            PerformAndroidBuild("StrikeHero");
+        }
+
+        public static string PerformAndroidBuild(string apkName, bool isDevelopment = true)
+        {
+            BuildOptions opt = isDevelopment
+                ? (BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.ConnectWithProfiler)
+                : BuildOptions.None;
+            var path = string.Format("Apps/{0}/{1}.apk", "Android", apkName);
+            return PerformBuild(path, BuildTarget.Android, opt);
+        }
+
+        [MenuItem("KEngine/Clear PC PersitentDataPath")]
+        public static void ClearPersistentDataPath()
+        {
+            foreach (string dir in Directory.GetDirectories(KResourceModule.GetAppDataPath()))
             {
-                // TODO:
-                Logger.Log("TODO: {0}", dirPath);
-                //CBuildTools.DeleteLink(dirPath);
+                Directory.Delete(dir, true);
+            }
+            foreach (string file in Directory.GetFiles(KResourceModule.GetAppDataPath()))
+            {
+                File.Delete(file);
             }
         }
-  
+        [MenuItem("KEngine/Open PC PersitentDataPath Folder")]
+        public static void OpenPersistentDataPath()
+        {
+            System.Diagnostics.Process.Start(KResourceModule.GetAppDataPath());
+        }
+
+        [MenuItem("KEngine/Clear Prefs")]
+        public static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+            CBuildTools.ShowDialog("Prefs Cleared!");
+        }
     }
 
-    /// <summary>
-    /// 如果不存在，创建link，并执行callback，完成后删掉link
-    /// 如果存在，执行callback，完成后不删
-    /// </summary>
-    
-    public static void SymbolLinkResourceAndDoAction(Action doAction = null)
+    public class CSymbolLinkHelper
     {
-        var resourcePath = GetResourceExportPath();
-        var linkPath = GetLinkPath();
-        if (!Directory.Exists(linkPath))
+
+        public static string AssetBundlesLinkPath = "Assets/StreamingAssets/" + KEngine.AppEngine.GetConfig(CCosmosEngineDefaultConfig.StreamingBundlesFolderName) + "/"; // hold asset bundles
+        public static string GetLinkPath()
         {
-            SymbolLinkResource();
-
-            if (doAction != null)
-                doAction();
-
-            // TODO: DeleteLink
-            //CBuildTools.DeleteLink(linkPath);
+            if (!Directory.Exists(AssetBundlesLinkPath))
+                Directory.CreateDirectory(AssetBundlesLinkPath);
+            return AssetBundlesLinkPath + KResourceModule.BuildPlatformName + "/";
         }
-        else
-        {
-            if (doAction != null)
-                doAction();
 
-            Logger.LogWarning("[SymbolLinkTest]Exist so not link: {0}", resourcePath);
+        public static string GetResourceExportPath()
+        {
+            var resourcePath = CBuildTools.GetExportPath(EditorUserBuildSettings.activeBuildTarget, KResourceModule.Quality);
+            return resourcePath;
+        }
+
+        [MenuItem("KEngine/Symbol Link Builded Resource to StreamingAssets")]
+        public static void SymbolLinkResource()
+        {
+            //CSymbolLinkHelper.DeleteAllLinks(CSymbolLinkHelper.AssetBundlesLinkPath);
+
+            //var exportPath = GetResourceExportPath();
+            //var linkPath = GetLinkPath();
+
+            // TODO: 
+            //CBuildTools.SymbolLinkFolder(exportPath, linkPath);
+
+            Logger.Log("TODO:");
+            AssetDatabase.Refresh();
+        }
+
+        /// <summary>
+        /// 删除指定目录所有链接
+        /// </summary>
+        /// <param name="assetBundlesLinkPath"></param>
+        private static void DeleteAllLinks(string assetBundlesLinkPath)
+        {
+            if (Directory.Exists(assetBundlesLinkPath))
+            {
+                foreach (var dirPath in Directory.GetDirectories(assetBundlesLinkPath))
+                {
+                    // TODO:
+                    Logger.Log("TODO: {0}", dirPath);
+                    //CBuildTools.DeleteLink(dirPath);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 如果不存在，创建link，并执行callback，完成后删掉link
+        /// 如果存在，执行callback，完成后不删
+        /// </summary>
+
+        public static void SymbolLinkResourceAndDoAction(Action doAction = null)
+        {
+            var resourcePath = GetResourceExportPath();
+            var linkPath = GetLinkPath();
+            if (!Directory.Exists(linkPath))
+            {
+                SymbolLinkResource();
+
+                if (doAction != null)
+                    doAction();
+
+                // TODO: DeleteLink
+                //CBuildTools.DeleteLink(linkPath);
+            }
+            else
+            {
+                if (doAction != null)
+                    doAction();
+
+                Logger.LogWarning("[SymbolLinkTest]Exist so not link: {0}", resourcePath);
+            }
         }
     }
+
 }
