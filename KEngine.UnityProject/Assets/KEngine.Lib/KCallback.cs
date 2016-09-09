82ad710776ab48d3b3f24f6b2cd2de96e8c5c4a8
Mrkelly <23110388@qq.com>
2016-09-09 10:54:01 +0800

    remove Callback's dependency

--- KEngine.UnityProject/Assets/KEngine.Lib/KCallback.cs
+++ KEngine.UnityProject/Assets/KEngine.Lib/KCallback.cs
@@ -1,8 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
-
+﻿
 /// <summary>
 /// KEngine standard callback, like nodejs 's standard callback
 /// </summary>
 /// <param name="args"></param>
-public delegate void KCallback(object[] data, string err = null);
+public delegate void KCallback(object[] data, System.Exception err = null);
